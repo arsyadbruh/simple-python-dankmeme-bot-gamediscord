3da7120597dd90df0b5217e928879e1282bd543f
arsyadbruh <71438511+arsyadbruh@users.noreply.github.com>
2020-12-11 17:53:47 +0700

    script python

---
+++ bot-currecny-dankmeme.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+#import random
+
+time.sleep(3)
+listMeme = ['n','e','r','d']
+
+while True:
+    pyautogui.typewrite("pls hunt")
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.typewrite("pls fish")
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.typewrite("pls beg")
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.typewrite("pls pm")
+    pyautogui.press("enter")
+    time.sleep(2)
+    #pyautogui.typewrite(random.choice(listMeme)) #kalau random pakai ini
+    pyautogui.typewrite('r') #karena saya untungnya di repost (r)
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.typewrite("pls dep all")
+    pyautogui.press("enter")
+    time.sleep(52)
