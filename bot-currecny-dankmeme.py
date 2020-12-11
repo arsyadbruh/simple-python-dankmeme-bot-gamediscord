3d234294e2bb29179e88c636eac7b235060f78bc
arsyadbruh <71438511+arsyadbruh@users.noreply.github.com>
2020-12-11 21:50:22 +0700

    added some helpful comment

--- bot-currecny-dankmeme.py
+++ bot-currecny-dankmeme.py
@@ -1,10 +1,16 @@
+# Script by Muhamad Arsyad
+# Github : github.com/arsyadbruh
+# uncomment random untuk memakai random choice post meme
+# pip install pyautogui
+# setelah run, hanya punya waktu 3 detik untuk klik channel discord untuk dankmeme currency
+# ubah aturan waktu pada time.sleep()
+
 import pyautogui
 import time
 #import random
 
-time.sleep(3)
 #listMeme = ['n','e','r','d']
-
+time.sleep(3)
 while True:
     pyautogui.typewrite("pls hunt")
     pyautogui.press("enter")
@@ -18,10 +24,13 @@ while True:
     pyautogui.typewrite("pls pm")
     pyautogui.press("enter")
     time.sleep(2)
-    #pyautogui.typewrite(random.choice(listMeme)) #kalau random pakai ini
-    pyautogui.typewrite('r') #karena saya untungnya di repost (r)
+    # uncomment dibawah ini jika ingin menggunakan random choice post meme
+    #pyautogui.typewrite(random.choice(listMeme)) 
+    pyautogui.typewrite('r') #comment line ini jika ingin menggunakan random choice
     pyautogui.press("enter")
     time.sleep(2)
     pyautogui.typewrite("pls dep all")
     pyautogui.press("enter")
     time.sleep(52)
+
+#have fun waktu di set selama +-1 menit
